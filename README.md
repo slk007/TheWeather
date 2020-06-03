9db2a4cb341df39e539f8fae17f3880aec4c8d69
slk007 <shubham.soni.lko@gmail.com>
2020-06-03 12:28:05 +0530

    req

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TheWeather
+
+* Inside
+    * API
+    * CRUD
+
--- weather/models.py
+++ weather/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class City(models.Model):
     
-    name = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
